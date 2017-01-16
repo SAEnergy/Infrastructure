a18be7a440ab871d89a986b5f772406df688f1be
Robert Bean <robert.bean@saenergy.com>
2017-01-16 14:08:21 -0600

    Split history chart into duration and counts.

--- Src/Client/Scheduler.Plugin/JobHistoryViewModel.cs
+++ Src/Client/Scheduler.Plugin/JobHistoryViewModel.cs
@@ -15,16 +15,49 @@ namespace Scheduler.Plugin
 {
     public class JobHistoryViewModel : ViewModelBase<ISchedulerHost>, ISchedulerCallback
     {
-        public PlotModel Data { get; private set; }
+        public PlotModel DurationData { get; private set; }
+        public PlotModel CountData { get; private set; }
         private int _id;
 
         public JobHistoryViewModel(ViewBase parent, int jobID) : base(parent)
         {
             _id = jobID;
-            Data = new PlotModel();
-            Data.Axes.Add(new DateTimeAxis() { Position = AxisPosition.Bottom, Key = "Date" });
-            Data.Axes.Add(new LinearAxis() { Position = AxisPosition.Right, Key = "Counts" });
-            Data.Axes.Add(new TimeSpanAxis() { Position = AxisPosition.Left, Key = "Duration" });
+            DurationData = new PlotModel();
+            DurationData.Axes.Add(new DateTimeAxis() { Position = AxisPosition.Bottom, Key = "Date" });
+            DurationData.Axes.Add(new TimeSpanAxis() { Position = AxisPosition.Left, Key = "Duration" });
+            CountData = new PlotModel();
+            CountData.Axes.Add(new DateTimeAxis() { Position = AxisPosition.Bottom, Key = "Date" });
+            CountData.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, Key = "Counts" });
+
+            bool isInternalChange = false;
+            Axis d1 = DurationData.Axes.First(a => a.Key == "Date");
+            Axis d2 = CountData.Axes.First(a => a.Key == "Date");
+
+            d1.AxisChanged += (s, e) =>
+            {
+                if (isInternalChange)
+                {
+                    return;
+                }
+
+                isInternalChange = true;
+                d2.Zoom(d1.ActualMinimum, d1.ActualMaximum);
+                CountData.InvalidatePlot(false);
+                isInternalChange = false;
+            };
+
+            d2.AxisChanged += (s, e) =>
+            {
+                if (isInternalChange)
+                {
+                    return;
+                }
+
+                isInternalChange = true;
+                d1.Zoom(d2.ActualMinimum, d2.ActualMaximum);
+                DurationData.InvalidatePlot(false);
+                isInternalChange = false;
+            };
         }
 
         protected override void OnConnect(ISubscription source)
@@ -36,7 +69,8 @@ namespace Scheduler.Plugin
         protected override void OnDisconnect(ISubscription source, Exception error)
         {
             base.OnDisconnect(source, error);
-            Data.Series.Clear();
+            DurationData.Series.Clear();
+            CountData.Series.Clear();
         }
 
         public void JobAdded(JobConfiguration job) { }
@@ -55,35 +89,34 @@ namespace Scheduler.Plugin
                 {
                     foreach (PropertyInfo prop in stat.GetType().GetProperties())
                     {
-                        if (prop.PropertyType != typeof(int) && prop.PropertyType != typeof(TimeSpan)) { continue; }
                         if (prop.Name.Contains("ID")) { continue; }
-                        LineSeries ser = (LineSeries)Data.Series.FirstOrDefault(f => f.Title == prop.Name);
-                        if (ser == null)
+
+                        if (prop.PropertyType == typeof(int))
                         {
-                            ser = new LineSeries();
-                            ser.Title = prop.Name;
-                            ser.XAxisKey = "Date";
-                            if (prop.PropertyType == typeof(TimeSpan))
+                            LineSeries ser = (LineSeries)CountData.Series.FirstOrDefault(f => f.Title == prop.Name);
+                            if (ser == null)
                             {
-                                ser.YAxisKey = "Duration";
+                                ser = new LineSeries();
+                                ser.Title = prop.Name;
+                                CountData.Series.Add(ser);
                             }
-                            else
-                            {
-                                ser.YAxisKey = "Counts";
-                            }
-                            Data.Series.Add(ser);
+                            ser.Points.Add(new DataPoint(DateTimeAxis.ToDouble(stat.StartTime), (int)prop.GetValue(stat)));
                         }
                         if (prop.PropertyType == typeof(TimeSpan))
                         {
+                            LineSeries ser = (LineSeries)DurationData.Series.FirstOrDefault(f => f.Title == prop.Name);
+                            if (ser == null)
+                            {
+                                ser = new LineSeries();
+                                ser.Title = prop.Name;
+                                DurationData.Series.Add(ser);
+                            }
                             ser.Points.Add(new DataPoint(DateTimeAxis.ToDouble(stat.StartTime), TimeSpanAxis.ToDouble((TimeSpan)prop.GetValue(stat))));
-                        }
-                        else
-                        {
-                            ser.Points.Add(new DataPoint(DateTimeAxis.ToDouble(stat.StartTime), (int)prop.GetValue(stat)));
                         }
                     }
                 }
-                Data.InvalidatePlot(true);
+                DurationData.InvalidatePlot(true);
+                CountData.InvalidatePlot(true);
             });
         }
     }
